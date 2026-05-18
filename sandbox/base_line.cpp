8c6da6400f680f661dae0f633c7632ce2917f591
Ziv Perry <ziv.perry@gmail.com>
2026-05-18 14:29:20 +0300

    fix: baseline

---
+++ sandbox/base_line.cpp
@@ -0,0 +1,39 @@
+#include "raylib.h"
+#include "raytiles/raytiles.h"
+
+int main() {
+    SetTraceLogLevel(LOG_ERROR);
+
+    InitWindow(800, 600, "raytiles");
+    double t = GetTime();
+
+    raytiles::world_config world;
+    raytiles::streaming_config streaming;
+    raytiles::rendering_config rendering;
+    raytiles::pool_config pool_conf;
+
+    raytiles::streamer streamer(world, streaming, rendering, pool_conf);
+
+    Camera3D camera;
+    camera.position = Vector3{2000.0f, 5000.0f, 2000.0f};
+    camera.target = Vector3{3000.0f, 4750.0f, 3000.0f};
+    camera.up = Vector3{0.0f, 1.0f, 0.0f};
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    // loading loop
+    for (;;) {
+        streamer.update(camera);
+        if (!streamer.is_loading()) break;
+
+        const auto loading = streamer.get_loading();
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText(TextFormat("Loading... %.1f%%", loading * 100.0f), 350, 350, 50, WHITE);
+        EndDrawing();
+    }
+
+    TraceLog(LOG_ERROR, "------------------ initial loading took %.2f seconds", GetTime() - t);
+    CloseWindow();
+    return 0;
+}
